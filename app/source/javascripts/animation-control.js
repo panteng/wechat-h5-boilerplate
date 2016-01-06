5f1b64f1e7291f1ad2603b5fc8d5a18be3af2529
Chris Pan <chrispan.pt@gmail.com>
2016-01-06 10:31:41 +0800

    Finish animation-control.

---
+++ app/source/javascripts/animation-control.js
@@ -0,0 +1,38 @@
+var $ = jQuery = require('jquery');
+
+module.exports = {
+    initAnimationItems: function() {
+        $('.animated').each(function () {
+            var aniDuration, aniDelay;
+            $(this).attr('data-origin-class', $(this).attr('class'));
+            aniDuration = $(this).data('ani-duration');
+            aniDelay = $(this).data('ani-delay');
+            $(this).css({
+                'visibility': 'hidden',
+                'animation-duration': aniDuration,
+                '-webkit-animation-duration': aniDuration,
+                'animation-delay': aniDelay,
+                '-webkit-animation-delay': aniDelay
+            });
+        });
+    },
+
+    execAnimation: function (swiper) {
+        this.clearAnimation();
+        var aniItems = swiper.slides[swiper.activeIndex].querySelectorAll('.animated');
+        $(aniItems).each(function () {
+            var aniName;
+            $(this).css({ 'visibility': 'visible' });
+            aniName = $(this).data('ani-name');
+            $(this).addClass(aniName);
+        });
+
+    },
+
+    clearAnimation: function () {
+        $('.animated').each(function () {
+            $(this).css({ 'visibility': 'hidden' });
+            $(this).attr('class', $(this).data('origin-class'));
+        });
+    }
+}
